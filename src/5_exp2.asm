af42ae41a366af82f4331b3f202c13e74a1e06fb
yonh <azssjli@163.com>
2016-04-02 22:17:35 +0800

    add 5exp2.asm

--- src/5_exp2.asm
+++ src/5_exp2.asm
@@ -1,17 +1,16 @@
-;;; 第五章练习1
+;;; 第五章练习2
 ;;; 将内存往内存0200:0~0200:23f 写入 0~23f
+;;; 只能使用9行代码, 使用bl替代al的方式减少2行代码
 
 assume cs:code
 code segment
 	mov ax, 0200h
 	mov ds, ax
 
-	mov ax, 0
 	mov bx, 0
 	mov cx , 40h		;40h = 64
 	
-	s: mov [bx], al
-	inc ax
+	s: mov [bx], bl
 	inc bx
 	loop s
 	
